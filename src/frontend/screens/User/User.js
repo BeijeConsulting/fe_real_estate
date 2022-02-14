b4b26aceb14e65e05a0137e51555fd4f4029e3e2
emidimar <dimariemilio96@gmail.com>
2022-02-14 17:15:26 +0100

    edit languages button and logout

--- src/frontend/screens/User/User.js
+++ src/frontend/screens/User/User.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Outlet, Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import Navbar from '../../components/Navbar/Navbar';
 
 class User extends Component {
     constructor(props) {
@@ -12,30 +13,32 @@ class User extends Component {
     }
     render() {
         return (
-            <div className='flex'>
+            <>
+                <Navbar />
+                <div className='flex'>
+                    <sidebar className="flex flex-col w-3/12 h-screen bg-secondary">
+                        <nav className='flex flex-col p-10 text-lg font-primary color-primary'>
+                            <Link className={"m-2 link-menu"} to={"user/edit-profile"}>Modifica profilo</Link>
+                            <Link className={"m-2 link-menu"} to={"user/post-advs"}>Annunci pubblicati</Link>
+                            <Link className={"m-2 link-menu"} to={"user/save-advs"}>Annunci salvati</Link>
+                            <Link className={"m-2 link-menu"} to={"user/assess-building"}>Valuta la tua casa</Link>
+                        </nav>
+                        <div className='flex flex-row ml-4'>
+                            <button> IT </button>
+                            <button> EN </button>
+                            <FontAwesomeIcon icon={faArrowRightFromBracket} />
+                        </div>
+                    </sidebar>
+                    <section className='flex flex-col font-primary'>
+                        <h1 className='text-3xl font-bold m-4'>
+                            Benvenuto @utente
+                        </h1>
 
-                <sidebar className="flex flex-col w-3/12 h-screen bg-secondary">
-                    <nav className='flex flex-col p-10 text-lg font-primary color-primary'>
-                        <Link className={"m-2 link-menu"} to={"user/edit-profile"}>Modifica profilo</Link>
-                        <Link className={"m-2 link-menu"} to={"user/post-advs"}>Annunci pubblicati</Link>
-                        <Link className={"m-2 link-menu"} to={"user/save-advs"}>Annunci salvati</Link>
-                        <Link className={"m-2 link-menu"} to={"user/assess-building"}>Valuta la tua casa</Link>
-                    </nav>
-                    <div className='flex flex-row'>
-                        <button> IT </button>
-                        <button> EN </button>
-                        <FontAwesomeIcon icon={faArrowRightFromBracket} />
-                    </div>
-                </sidebar>
-                <section className='flex flex-col font-primary'>
-                    <h1 className='text-3xl font-bold m-4'>
-                        Benvenuto @utente
-                    </h1>
+                        <Outlet />
+                    </section>
 
-                    <Outlet />
-                </section>
-
-            </div>
+                </div>
+            </>
         )
     }
 }
