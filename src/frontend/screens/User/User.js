3c0e3253768376924b5aaf177168242611b08eb1
emidimar <dimariemilio96@gmail.com>
2022-02-14 17:57:46 +0100

    add new icon

--- src/frontend/screens/User/User.js
+++ src/frontend/screens/User/User.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Outlet, Link } from 'react-router-dom';
 import Navbar from '../../components/Navbar/Navbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenToSquare, faBookmark } from '@fortawesome/free-solid-svg-icons'
+import { faPenToSquare, faBookmark, faHouseChimney } from '@fortawesome/free-solid-svg-icons'
 
 import "./user.css"
 
@@ -18,18 +18,23 @@ class User extends Component {
             <>
                 <Navbar />
                 <div className='flex'>
-                    <sidebar className="flex flex-col w-3/12 h-screen bg-secondary">
+                    <div className="flex flex-col w-3/12 h-screen bg-secondary">
                         <nav className='flex flex-col p-10 text-lg font-primary color-primary'>
                             <Link className={"m-2 link-menu"} to={"user/edit-profile"}>
-                                <FontAwesomeIcon className={"mr-1"} icon={faPenToSquare} />
-                                Modifica profilo</Link>
+                                <FontAwesomeIcon className={"mr-2"} icon={faPenToSquare} />
+                                Modifica profilo
+                            </Link>
                             <Link className={"m-2 link-menu"} to={"user/post-advs"}>Annunci pubblicati</Link>
                             <Link className={"m-2 link-menu"} to={"user/save-advs"}>
-                                <FontAwesomeIcon className={"mr-1"} icon={faBookmark} />
-                                Annunci salvati</Link>
-                            <Link className={"m-2 link-menu"} to={"user/assess-building"}>Valuta la tua casa</Link>
+                                <FontAwesomeIcon className={"mr-2"} icon={faBookmark} />
+                                Annunci salvati
+                            </Link>
+                            <Link className={"m-2 link-menu"} to={"user/assess-building"}>
+                                <FontAwesomeIcon className={"mr-2"} icon={faHouseChimney} />
+                                Valuta la tua casa
+                            </Link>
                         </nav>
-                    </sidebar>
+                    </div>
                     <section className='flex flex-col font-primary'>
                         <h1 className='text-3xl font-bold m-4'>
                             Benvenuto @utente
