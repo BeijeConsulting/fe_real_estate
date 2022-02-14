e010cf9089f1322ff18e44310494d3b238cbd93f
emidimar <dimariemilio96@gmail.com>
2022-02-14 17:49:59 +0100

    add hover in link nav and icon

--- src/frontend/screens/User/User.js
+++ src/frontend/screens/User/User.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import { Outlet, Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 import Navbar from '../../components/Navbar/Navbar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare, faBookmark } from '@fortawesome/free-solid-svg-icons'
+
+import "./user.css"
 
 class User extends Component {
     constructor(props) {
@@ -18,16 +20,15 @@ class User extends Component {
                 <div className='flex'>
                     <sidebar className="flex flex-col w-3/12 h-screen bg-secondary">
                         <nav className='flex flex-col p-10 text-lg font-primary color-primary'>
-                            <Link className={"m-2 link-menu"} to={"user/edit-profile"}>Modifica profilo</Link>
+                            <Link className={"m-2 link-menu"} to={"user/edit-profile"}>
+                                <FontAwesomeIcon className={"mr-1"} icon={faPenToSquare} />
+                                Modifica profilo</Link>
                             <Link className={"m-2 link-menu"} to={"user/post-advs"}>Annunci pubblicati</Link>
-                            <Link className={"m-2 link-menu"} to={"user/save-advs"}>Annunci salvati</Link>
+                            <Link className={"m-2 link-menu"} to={"user/save-advs"}>
+                                <FontAwesomeIcon className={"mr-1"} icon={faBookmark} />
+                                Annunci salvati</Link>
                             <Link className={"m-2 link-menu"} to={"user/assess-building"}>Valuta la tua casa</Link>
                         </nav>
-                        <div className='flex flex-row ml-4'>
-                            <button> IT </button>
-                            <button> EN </button>
-                            <FontAwesomeIcon icon={faArrowRightFromBracket} />
-                        </div>
                     </sidebar>
                     <section className='flex flex-col font-primary'>
                         <h1 className='text-3xl font-bold m-4'>
