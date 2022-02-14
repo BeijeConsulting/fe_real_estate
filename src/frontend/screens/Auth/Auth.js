41a621d07335b948e6bd65441d525605b948f4db
Grazia Palombella <grazia.palombella@gmail.com>
2022-02-14 14:21:44 +0100

    Auth screen + Card UI, changed logo in public

--- src/frontend/screens/Auth/Auth.js
+++ src/frontend/screens/Auth/Auth.js
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
+import Card from '../../components/UI/Card/Card'
+import logo from '../../../common/assets/logo/logo.png'
 
 import { Outlet } from "react-router-dom";
 
 export default class Auth extends Component {
 	render() {
 		return (
-			<div>
-				{/* schermo blu */}
-				{/* header with navbar and logo below */}
+			<div className="bg-secondary h-screen ">
+				<header className="flex items-center justify-center gap-2 pt-5 pb-5">
+					<img src={logo} alt="logo" className="h-10 "/>
+					<h1 className="color-primary uppercase font-extrabold text-4xl">domus</h1>
+				</header>
 
-				{/* CARD WITH FORM */}
-				<div>
+				<div className="flex items-center justify-center">
+				<Card className="rounded flex items-center justify-center bg-white w-10/12">
 					{/* Login / Registration */}
 					<Outlet />
+				</Card>
 				</div>
 			</div>
 		);
