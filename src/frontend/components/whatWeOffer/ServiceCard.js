7d6103f434580b5663d6bad4f04bacf11e5b4cb4
Enrico <enrico.digrazia98@gmail.com>
2022-02-15 19:00:07 +0100

    services' ads added. Need complete searchbar to finish services' page.

--- src/frontend/components/whatWeOffer/ServiceCard.js
+++ src/frontend/components/whatWeOffer/ServiceCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Button from "../UI/Button/Button";
 import { useNavigate } from "react-router-dom";
 
 const ServiceCard = (props) => {
@@ -9,18 +10,30 @@ const ServiceCard = (props) => {
 
 	return (
 		<div
-			className={`h-48 white flex items-center justify-center rounded-xl overflow-hidden text-white`}
+			className={`h-60 white flex ${
+				props.reverseRow ? "flex-row-reverse" : ""
+			} items-center justify-center rounded-xl overflow-hidden`}
 		>
 			<div
-				className={`h-full bg-secondary w-96 p-3 flex flex-col items-start justify-between ${props.classNameRedirectSection}`}
+				className={`h-full bg-secondary w-screen max-w-lg p-6 flex flex-col 
+                ${
+				props.reverseRow ? "items-end" : "items-start"
+			}  justify-between ${props.classNameRedirectSection}`}
 			>
-				<p>{props.title}</p>
-				<button onClick={goTo(props.redirectPath)}>
-					{props.buttonText}
-				</button>
+				<p
+					className={`text-white text-2xl 
+                        ${props.reverseRow ? "text-right" : "text-left"}
+                        font-bold uppercase w-80 whitespace-pre-wrap`}
+				>
+					{props.title}
+				</p>
+				<Button
+					label={props.buttonText}
+					callback={goTo(props.redirectPath)}
+				/>
 			</div>
 			<div
-				className={`h-full w-52 p-3`}
+				className={`h-full w-screen max-w-xs p-3`}
 				style={{ backgroundColor: "#403F3F" }}
 			>
 				{props.children}
@@ -33,6 +46,7 @@ ServiceCard.defaultProps = {
 	buttonText: "Awesome button",
 	classNameRedirectSection: "",
 	redirectPath: "/not-found",
+	reverseRow: false,
 	title: "Awesome title",
 };
 
@@ -40,6 +54,7 @@ ServiceCard.propTypes = {
 	buttonText: PropTypes.string,
 	classNameRedirectSection: PropTypes.string,
 	redirectPath: PropTypes.string,
+	reverseRow: PropTypes.bool,
 	title: PropTypes.string,
 };
 
--- src/frontend/screens/Home/WhatWeOffer.js
+++ src/frontend/screens/Home/WhatWeOffer.js
@@ -5,30 +5,60 @@ import Footer from "../../components/Footer/Footer";
 import ServiceCard from "../../components/whatWeOffer/ServiceCard";
 
 const CLASSES = {
-	CARD: "flex flex-col items-start mt-4 mb-2",
-	PRE_CARD_TEXT: "ml-2 mb-6 uppercase",
+	CARD: "flex flex-col items-start mt-4 mb-2 my-8",
+	PRE_CARD_TEXT: "ml-2 mb-6 uppercase font-semibold",
+	CARD_INFO:
+		"flex flex-col items-start justify-evenly text-sm text-white font-bold",
 };
 
 const WhatWeOffer = () => {
 	return (
 		<div className="h-screen flex flex-col justify-between items-center">
-			<header className="w-screen mb-3">
+			<header className="w-screen mb-4">
 				<NavBar />
 			</header>
-			<div className="flex flex-col items-center">
-				<p>Cerca un immobile</p>
-				<p>Questa è una searchbar</p>
+			<div className="flex flex-col items-center mt-4">
+				<p
+					className={
+						"font-bold text-black text-xl  uppercase "
+					}
+				>
+					Cerca un immobile
+				</p>
+				<input
+					placeholder="Searchbar"
+					type="text"
+					style={{
+						border: "1px solid black",
+						padding: 3,
+					}}
+				/>
 			</div>
 			<div className={CLASSES.CARD}>
 				<p className={CLASSES.PRE_CARD_TEXT}>
 					Sei un privato o un'agenzia immobiliare?
 				</p>
 				<ServiceCard
+					buttonText={"Inserisci"}
 					classNameRedirectSection={
 						"what-we-offer__insert-ad"
 					}
+					title={"inserisci ora un nuovo annuncio!"}
 				>
-					<p>Hai bisogno di un aiuto?</p>
+					<div className={CLASSES.CARD_INFO}>
+						<p className={"text-xl uppercase"}>
+							Hai bisogno di un aiuto?
+						</p>
+						<p className="">
+							<br />
+							Chiama il numero <br /> 0289623520!{" "}
+							<br />
+							da lunedì a venerdì - dalle <br />{" "}
+							09.00 alle 21.00
+							<br /> <br />
+							Oppure scrivi a: domus@casa.it
+						</p>
+					</div>
 				</ServiceCard>
 			</div>
 			<div className={CLASSES.CARD}>
@@ -37,11 +67,33 @@ const WhatWeOffer = () => {
 					proprietà?
 				</p>
 				<ServiceCard
+					buttonText={"Valuta"}
 					classNameRedirectSection={
 						"what-we-offer__assess-property"
 					}
+					reverseRow={true}
+					title={"valuta ora una tua proprieta'!"}
 				>
-					<p>Hai bisogno di un aiuto?</p>
+					<div className={`${CLASSES.CARD_INFO}`}>
+						<p
+							className={
+								"text-xl uppercase text-center w-full"
+							}
+						>
+							Tante novita'!
+						</p>
+						<p className="ml-4">
+							<br />
+							Da ora puoi valutare la tua proprietà
+							direttamente sul nostro sito
+							<br />
+							<br />
+							Oppure contattando <br />
+							un nostro operatore al numero:
+							<br />
+							0289623520.
+						</p>
+					</div>
 				</ServiceCard>
 			</div>
 			<footer className="w-screen mt-3">
