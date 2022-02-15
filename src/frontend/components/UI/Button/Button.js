36b4e7dcc1e0a20b81f66084cb437bab3c892605
Grazia Palombella <grazia.palombella@gmail.com>
2022-02-15 16:56:40 +0100

    Input e BTN UI

--- src/frontend/components/UI/Button/Button.js
+++ src/frontend/components/UI/Button/Button.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import PropTypes from "prop-types";
 
-const Button = () => {
+
+const Button = (props) => {
+  let classColor= props.type === 'primary' ? 'bg-primary' : 'bg-secondary text-white font-primary rounded-full flex py-1 px-2 cursor-pointer'
   return (
-    <div>Button</div>
+    <div className={classColor}>
+      <button className={props.classLabel}>{props.label}</button>
+      <div>
+        {props.image}
+      </div>
+    </div>
   )
 }
+
+
+Button.defaultProps = {
+  className: "bg-secondary text-white font-primary rounded-full flex py-1 px-2 cursor-pointer",
+  classLabel: 'font-bold uppercase mr-4',
+  image: ''
+};
+
+Button.propTypes = {
+  label: PropTypes.string,
+  className: PropTypes.string,
+  image: PropTypes.node
+};
 
 export default Button
--- src/frontend/screens/Auth/Login.js
+++ src/frontend/screens/Auth/Login.js
@@ -5,8 +5,9 @@ import { setUser } from "../../../redux/ducks/userMeDuck";
 
 import Input from "../../components/UI/Input/Input";
 import Checkbox from "../../components/UI/Checkbox/Checkbox";
+import Button from "../../components/UI/Button/Button";
 
-import {faPerson} from "@fortawesome/free-solid-svg-icons"
+import { faUser, faLock, faCircleArrowRight } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class Login extends Component {
@@ -57,42 +58,47 @@ class Login extends Component {
 					<img src="" alt="login" />
 				</div>
 				<form className="flex flex-col items-center ">
-					<h1 className="capitalise font-extrabold text-4xl">
+					<h1 className="capitalise font-primary font-extrabold text-4xl">
 						Area Privata
 					</h1>
-					<p className="font-light text-sm w-2/3 text-center">
+					<p className="font-primary font-light text-sm w-2/3 text-center">
 						Inserisci le credenziali ed accedi alla tua
 						area privata di Domus
 					</p>
 
 					<Input
-						image={<FontAwesomeIcon icon={faPerson} />}
+						image={<FontAwesomeIcon icon={faUser} />}
 						type="email"
 						onChange={this.onChangeEmail}
 						placeholder="Email"
-						className="rounded bg-secondary text-white pt-2 pb-2 pl-2 pr-2 mb-2 mt-8"
 					/>
 
 					<Input
-						image={<FontAwesomeIcon icon={faPerson} />}
+						image={<FontAwesomeIcon icon={faLock} />}
 						type="password"
 						onChange={this.onChangePassword}
 						placeholder="Password"
-						className="rounded bg-secondary text-white pt-2 pb-2 pl-2 pr-2 mb-3"
 					/>
 
-					<Checkbox
-						checked={this.state.rememberMe}
-						onChange={this.onChangeRememberMe}
-						label="Ricorda le mie credenziali"
-						className="font-black"
+
+					<div className="flex items-center gap-1 mt-2 mb-4">
+						<Checkbox
+							checked={this.state.rememberMe}
+							onChange={this.onChangeRememberMe}
+							label="Ricorda le mie credenziali"
+						/>
+					</div>
+
+
+					<Button
+						onClick={this.onClickLogin}
+						image={<FontAwesomeIcon icon={faCircleArrowRight} />}
+						label='Accedi'
+						type=''
 					/>
 
-					<button onClick={this.onClickLogin}>
-						Accedi
-					</button>
-					<p>Non hai un account?</p>
-					<Link to={"/auth/signup"}>Registrati adesso</Link>
+					<p className="font-primary mt-4">Non hai un account?</p>
+					<Link to={"/auth/signup"} className="font-primary">Registrati adesso</Link>
 				</form>
 			</>
 		);
