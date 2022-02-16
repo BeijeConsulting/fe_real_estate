db6f1dcf2210e6dd8b54bf635442d0fc00f4506f
Angelo Cipullo <angelo.cipullo@outlook.it>
2022-02-16 16:33:37 +0100

    Advertise List by Query

--- src/frontend/components/Search/Search.js
+++ src/frontend/components/Search/Search.js
@@ -2,22 +2,34 @@ import React, { useState } from 'react'
 import SearchSelect from './SearchSelect'
 import Button from '../UI/Button/Button'
 import { ADV_TYPES, BUILDING_TYPES } from '../../../common/utils/globalTypes'
+import { useNavigate } from 'react-router-dom'
 
 const Search = () => {
 
-  const [ query, setQuery ] = useState({
-    buildingType: {label:'Casa'},
-    advType: {label:'Vendita'},
-    location: {label:'Scegli dove..'}
+  let navigate = useNavigate()
+
+  const [query, setQuery] = useState({
+    buildingType: { label: 'Casa' },
+    advType: { label: 'Vendita' },
+    location: { label: 'Scegli dove..' }
   })
 
   let imgUrl = "https://www.lago.it/wp-content/uploads/2017/10/Lago-Appartamento-Store-Arnhem-1.jpg"
 
 
-  const setBuildingType = (value) => setQuery({...query, buildingType: value })
-  const setAdvType = (value) => setQuery({...query, advType: value })
-  const setLocation = (value) => setQuery({...query, location: value })
-  
+
+  const setBuildingType = (value) => setQuery({ ...query, buildingType: value })
+  const setAdvType = (value) => setQuery({ ...query, advType: value })
+  const setLocation = (value) => setQuery({ ...query, location: value })
+
+  const handleSubmit = () => {
+    let newUrl = 'adv/' 
+      + query.advType.value + "/"
+      + query.buildingType.value + "/"
+      + query.location.label
+
+    navigate(newUrl.toLowerCase())
+  }
 
   return (
     <div className='select-none flex-1 flex flex-col justify-center items-center'>
@@ -45,16 +57,17 @@ const Search = () => {
         />
       </div>
 
-      <Button 
+      <Button
         type="primary"
         size={26}
         label="VEDI 3 CASE"
         marginTop={25}
+        onClick={handleSubmit}
       />
 
 
       {/* BACKGROUND OVERLAY + BG */}
-      <div className='flex-1 absolute inset-0 -z-20' style={{ backgroundImage: `url(${imgUrl})`, backgroundSize: 'cover', backgroundPosition: 'center' }}/>
+      <div className='flex-1 absolute inset-0 -z-20' style={{ backgroundImage: `url(${imgUrl})`, backgroundSize: 'cover', backgroundPosition: 'center' }} />
       <div className='absolute inset-0 flex-1 bg-black opacity-70 -z-10' />
     </div>
   )
--- src/frontend/screens/Advertisement/AdvList.js
+++ src/frontend/screens/Advertisement/AdvList.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const AdvList = () => {
   return (
-    <div>AdvList</div>
+    <div>
+        
+    </div>
   )
 }
 
