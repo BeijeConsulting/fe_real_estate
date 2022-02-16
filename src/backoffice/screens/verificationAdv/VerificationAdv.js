8e1027ef86e1446ed7a87d12a011a398456594f9
Alessandro Veneziano <aveneziano536@gmail.com>
2022-02-16 15:25:26 +0100

    fixing

---
+++ src/backoffice/screens/verificationAdv/VerificationAdv.js
@@ -0,0 +1,18 @@
+import "./verificationAdv.css";
+
+
+const VerificationAdv = () => {
+
+
+
+
+
+    return (
+        <div className="container-VerificationAdv">
+            io sono  VerificationAdv
+        </div>
+    )
+}
+
+export default VerificationAdv;
+
