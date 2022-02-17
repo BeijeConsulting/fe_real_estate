1fe2e02d8bde314f9dcbc53416c4c982c05434c2
Angelo Cipullo <angelo.cipullo@outlook.it>
2022-02-17 15:17:52 +0100

    Adv Card & Adv List UPDATES 2 + Input Fix

--- src/frontend/components/AdvCard/AdvCard.js
+++ src/frontend/components/AdvCard/AdvCard.js
@@ -25,29 +25,29 @@ const AdvCard = ( props ) => {
                 />
             </div>
 
-            <div onClick={props.onClick} className=' bg-white hover:bg-gray-50 transition cursor-pointer  p-4 flex-col flex flex-1'>
+            <div onClick={props.onClick} className=' relative bg-white hover:bg-gray-50 transition cursor-pointer  p-4 flex-col flex flex-1'>
                 {/* INFO-RIGHT */}
                 <p className='font-primary font-semibold text-xl text-gray-700'>{props.city}</p>
                 <p className='block font-primary font-bold text-3xl '>{props.title}</p>
                 <p className='font-primary'>{props.description.slice(0, 90) + ".."}</p>
+
+                <p>3 locali • 4 mq</p>
+                <p className='bottom-4 right-6 absolute text-4xl font-primary font-bold'>{props.price} <span className='text-lg'>€</span></p>
             </div>
         </Card>
     )
 }
 
-let description = "Lorem ipsum dolor sit amet, consectetur adipiscing elite Vestibulum ultricies suscipit rhoncus. Curabitur sed feugiat mi, sed mollis tortor. Duis aliquet dapibus nulla non venenatis. Duis rutrum mauris vitae lacus sagittis facilisis. Mauris vehicula nulla quis interdum tempor. "
-
-let photos = [
-    "https://www.cinematographe.it/wp-content/uploads/2021/01/shrek.jpg",
-    "https://www.lascimmiapensa.com/wp-content/uploads/2021/11/Shrek.jpg"
-]
-
 AdvCard.defaultProps = {
     id: 0,
     title:'BILOCALE IN PERIFERIA',
-    description: description,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elite Vestibulum ultricies suscipit rhoncus. Curabitur sed feugiat mi, sed mollis tortor. Duis aliquet dapibus nulla non venenatis. Duis rutrum mauris vitae lacus sagittis facilisis. Mauris vehicula nulla quis interdum tempor. "    ,
     city:'Milano',
-    photos: photos,
+    price: 1000,
+    photos:[
+        "https://www.cinematographe.it/wp-content/uploads/2021/01/shrek.jpg",
+        "https://www.lascimmiapensa.com/wp-content/uploads/2021/11/Shrek.jpg"
+    ],
     authorName:"Jessica Beije",
     authorAvatarUrl:"https://bit.ly/3GYboVJ"
 }
--- src/frontend/components/Filters/Filters.js
+++ src/frontend/components/Filters/Filters.js
@@ -1,22 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '../../components/UI/Card/Card'
 import { Slider } from 'antd'
-
+import Button from '../UI/Button/Button'
 
 const Filters = () => {
+
+    const [filters, setFilters] = useState({
+        minPrice: 0,
+        maxPrice: 0,
+
+    })
+
+    const handlePriceFilter = (val) => {
+        setFilters({ ...filters, minPrice: val[0], maxPrice: val[1] })
+    }
+
     return (
-        <div className='flex-1 md:block hidden'>
-            <Card className='p-4 flex-col'>
-                <p className='font-primary font-bold text-2xl'>Filtri</p>
-                <p>Prezzo Minimo - Prezzo Massimo</p>
-                <Slider
-                    range
-                    min={0}
-                    max={1000}
-                    defaultValue={[200, 800]}
-                />
-            </Card>
-        </div>
+
+        <Card className='p-4 flex-col'>
+            <p className='font-primary font-bold text-2xl'>Filtri</p>
+
+
+            {/* MIN AND MAX PRICES */}
+            <div className='flex justify-between'>
+                <div className='flex flex-col'>
+                    <p>Prezzo Minimo</p>
+                    <input
+                        className='border-2 text-center'
+                        value={filters.minPrice + " €"}
+                    />
+                </div>
+                <div className='flex flex-col'>
+                    <p>Prezzo Massimo</p>
+                    <input
+                        className='border-2 text-center'
+                        value={filters.maxPrice + " €"}
+                    />
+                </div>
+            </div>
+            <Slider
+                step={50}
+                onAfterChange={handlePriceFilter}
+                range
+                min={0}
+                max={1000}
+                defaultValue={[200, 800]}
+            />
+
+            <Button
+                className='w-3/4 mx-auto mt-4'
+                label="Applica Filtri"
+            />
+        </Card>
+
     )
 }
 
