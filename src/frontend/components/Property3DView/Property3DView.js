b1d25733892fe6d891ff90552cd934590dbacd62
Enrico <enrico.digrazia98@gmail.com>
2022-02-17 09:36:57 +0100

    structuring component

---
+++ src/frontend/components/Property3DView/Property3DView.js
@@ -0,0 +1,17 @@
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+class Property3DView extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {};
+	}
+
+	render() {
+		return <div>Property3DView</div>;
+	}
+}
+
+export default Property3DView;
