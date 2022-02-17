b363afe41b97195996290ccef08ea285732d1571
Angelo Cipullo <angelo.cipullo@outlook.it>
2022-02-17 15:58:16 +0100

    FIX DA MERGE DI EMILIO

--- src/frontend/screens/Advertisement/AdvList.js
+++ src/frontend/screens/Advertisement/AdvList.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import React from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 
 import Button from '../../components/UI/Button/Button'
 import Navbar from '../../components/Navbar/Navbar'
@@ -7,48 +7,55 @@ import AdvCard from '../../components/AdvCard/AdvCard'
 import Filters from '../../components/Filters/Filters'
 
 const AdvList = () => {
-	let { city, advType } = useParams();
-	let navigate = useNavigate();
 
-	let type = "";
+    let { city, advType } = useParams()
+    let navigate = useNavigate()
 
-	const handleNavigate = (dest) => () => {
-		navigate(dest)
-	}
+    let type = '';
 
-	switch (advType) {
-		case 'rent': type = "Affitto"; break;
-		case 'sell': type = "Vendita"; break;
-		case 'short_rent': type = "Affito Breve"; break;
-	}
+    const handleNavigate = (dest) => () => {
+        navigate(dest)
+    }
 
-	return (
-		<div className="min-h-screen bg-gray">
-			<Navbar />
-			<div className='max-w-6xl mx-auto mt-10 flex'>
-				<Button
-					iconPosition="left"
-					label="Torna alla Home"
-					type="secondary"
-					onClick={() => navigate('/')}
-				/>
-			</div>
+    switch (advType) {
+        case 'rent': type = "Affitto"; break;
+        case 'sell': type = "Vendita"; break;
+        case 'short_rent': type = "Affito Breve"; break;
+    }
 
 
-			<div className='max-w-5xl p-2 mx-auto'>
-				<p className='text-3xl font-bold'>Ho trovato 2 case in {type} a {city} </p>
+    return (
+        <div className='min-h-screen bg-gray'>
+            <Navbar />
 
-				<div className='flex'>
-					<div style={{ flex: 2 }}>
-						<AdvCard />
-					</div>
-					<div className='flex-1'>
-						right
-					</div>
-				</div>
-			</div >
-		</div >
-	)
+
+            <div className='max-w-6xl mx-auto mt-10 flex'>
+                <Button
+                    iconPosition="left"
+                    label="Torna alla Home"
+                    type="secondary"
+                    onClick={handleNavigate('/')}
+                />
+            </div>
+
+            <div className='max-w-5xl lg:max-w-6xl p-2 mx-auto'>
+                <p className='text-3xl font-bold'>Ho trovato 2 case in {type} a {city} </p>
+
+                <div className='flex mt-10 space-x-4'>
+                    <div style={{ flex: 2 }}>
+                        {/* CARD LIST HERE */}
+                        <AdvCard
+                            onClick={handleNavigate(`/adv/${1}`)}
+                        />
+                    </div>
+                    <div className='flex-1 md:block hidden'>
+                        <Filters />
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default AdvList
