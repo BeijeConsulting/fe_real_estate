62f3190ef324820cecc35c2c6ab59aea8e0c963b
RiccardoRana <riccardorana@hotmail.com>
2022-02-18 12:56:40 +0100

    Edit User Menu now is more fancy

--- src/frontend/screens/User/EditProfile.js
+++ src/frontend/screens/User/EditProfile.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Card from '../../components/UI/Card/Card';
 import Input from '../../components/UI/Input/Input';
-
+import Button from '../../components/UI/Button/Button';
 class EditProfile extends Component {
     render() {
         return (
@@ -27,6 +27,9 @@ class EditProfile extends Component {
                                 <h1 className="text-xl font-sans">Numero di telefono:</h1>
                                 <Input className={"rounded bg-secondary flex items-center text-white mt-8 px-2 py-2 border-b-2 border-amber-300 font-primary relative -ml-44"}/>
                             </div>
+                            <div className='mt-4'>
+                           
+                            </div>
                         </div>
                         <div className='flex flex-col p-4'>
                             <h1 className="text-lg font-semibold">Dati utente</h1>
@@ -43,11 +46,19 @@ class EditProfile extends Component {
                                 <Input className={"rounded bg-secondary flex items-center text-white mt-8 px-2 py-2 border-b-2 border-amber-300 font-primary relative -ml-24"}/>
                             </div>
                             <div className='flex flex-row p-4'>
-                                <h1 className="text-xl font-sans">value:</h1>
+                                <h1 className="text-xl font-sans">Value:</h1>
                                 <Input className={"rounded bg-secondary flex items-center text-white mt-8 px-2 py-2 border-b-2 border-amber-300 font-primary relative -ml-14"}/>
                             </div>
+                            <div className='mt-8 w-44 p-4 -ml-24 h-24'>
+                            <Button 
+                            label='Salva'
+                            />
+                            </div>
                         </div>
+                        
                     </div>
+                
+                
                 </Card>
             </div>
         )
