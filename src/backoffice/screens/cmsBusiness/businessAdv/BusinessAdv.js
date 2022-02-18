26c174e1441d049cc577e4f94ef35006b5f0f29e
Antonio Consales <ant.consales@gmail.com>
2022-02-18 12:03:08 +0100

    start business list adv

--- src/backoffice/screens/cmsBusiness/businessAdv/BusinessAdv.js
+++ src/backoffice/screens/cmsBusiness/businessAdv/BusinessAdv.js
@@ -1,3 +1,60 @@
-const BusinessAdv = () => <>io sono BusinessAdv</>
+import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
+// css
+import "./businessAdv.css"
+// ant design
 
-export default BusinessAdv
+// axios
+import { getAdvBusinessName } from '../../../../services/backoffice/advertisementApi';
+
+//redux
+import { connect } from "react-redux";
+
+// react router
+import { useParams } from "react-router-dom";
+
+const BusinessAdv = (props) => {
+    // hooks
+    const [advBusiness, setAdvBusiness] = useState([]);
+
+    //router
+    const param = useParams();
+    console.log('Param', param);
+
+    //axios
+    // const getListAdvForBusiness = async () => {
+    //     let resultApi = await getAdvBusinessName(props.admin.token, param.id)
+    //     let formatData = [formatDataFromApi(resultApi[0])]
+    //     setAdv(formatData);
+    // }
+
+    // fuunc search bar
+
+    // func List
+
+    //useEffect
+
+    useEffect(() => {
+
+    }, []);
+
+    return (
+        <>
+            {/* seachbar */}
+
+            {/* list ADV for Business name */}
+
+        </>
+    )
+}
+
+// proptypes
+BusinessAdv.propTypes = {}
+
+//redux
+const mapStateToProps = (state) => ({
+    admin: state.adminDuck.admin
+})
+export default connect(mapStateToProps)(BusinessAdv)
+
+
