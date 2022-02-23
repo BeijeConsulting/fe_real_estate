68889658ab5c64a04fc6c35ed08a861a3b8cc524
emidimar <dimariemilio96@gmail.com>
2022-02-23 09:16:55 +0100

    fixed font dashboard

--- src/frontend/screens/User/Dashboard.js
+++ src/frontend/screens/User/Dashboard.js
@@ -13,17 +13,19 @@ class Dashboard extends Component {
 
         }
     }
+
+
     render() {
         return (
             <>
-                <h1 className='text-3xl font-bold m-4'>
+                <h1 className='text-3xl font-bold m-4 md:text-4xl'>
                     Benvenuto @utente
                 </h1>
                 <div className='flex flex-col m-2 p-2'>
                     <div className='flex flex-row space-x-2'>
-                        <h1 className='text-xl font-bold'>Riepilogo utente</h1>
+                        <h1 className='text-2xl font-bold'>Riepilogo utente</h1>
                         <Link to={"edit-profile"}>
-                            <FontAwesomeIcon className={"mt-1 h-5 text-slate-900"} icon={faPenToSquare} />
+                            <FontAwesomeIcon className={"mt-1.5 h-5 text-slate-900"} icon={faPenToSquare} />
                         </Link>
                     </div>
                     <div className='flex flex-col m-2 text-lg '>
@@ -34,13 +36,13 @@ class Dashboard extends Component {
                     </div>
                 </div>
                 <div className='flex flex-col m-2 p-2'>
-                    <h1 className='text-xl font-bold'>Annunci salvati:</h1>
+                    <h1 className='text-3xl font-bold'>Annunci salvati:</h1>
                     <div className='max-w-lg max-h-xs'>
                         <AdvCard />
                     </div>
                 </div>
                 <div className='flex flex-col m-2 p-2'>
-                    <h1 className='text-xl font-bold'>I tuoi annunci:</h1>
+                    <h1 className='text-3xl font-bold'>I tuoi annunci:</h1>
                     <div className='max-w-lg max-h-xs'>
                         <AdvCard />
                     </div>
