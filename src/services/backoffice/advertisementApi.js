5b8a80cfabaef57d8f1ad295959845a6ee5c8238
antonio <ant.consales@gmail.com>
2022-02-25 15:32:08 +0100

    search work in progress

--- src/services/backoffice/advertisementApi.js
+++ src/services/backoffice/advertisementApi.js
@@ -1,137 +1,140 @@
-import { javaAcademyServiceInstance } from "../javaAcademyService"
-
-
+import { javaAcademyServiceInstance } from "../javaAcademyService";
+// GET
 export const getAllAds = async (token) => {
-    let newAdvList = null;
-    let headers = {
-        'Authorization': `Bearer ${token}`,
-    }
-    const result = await javaAcademyServiceInstance.get(
-        `/ads`,
-        { headers }
-    ).then((response) => {
-        newAdvList = response.data;
+  let newAdvList = null;
+  let headers = {
+    Authorization: `Bearer ${token}`,
+  };
+  const result = await javaAcademyServiceInstance
+    .get(`/advertisement`, { headers })
+    .then((response) => {
+      newAdvList = response.data;
     });
-    return newAdvList
+  return newAdvList;
 };
 
 export const getAllAdsPaginations = async (token, pageId, total) => {
-    let advListPage = null;
-    let headers = {
-        'Authorization': `Bearer ${token}`,
-    }
-    const result = await javaAcademyServiceInstance.get(
-        `/adv/pages/${pageId}/${total}`,
-        { headers }
-    ).then((response) => {
-        advListPage = response.data;
+  let advListPage = null;
+  let headers = {
+    Authorization: `Bearer ${token}`,
+  };
+  const result = await javaAcademyServiceInstance
+    .get(`/advertisement/pages/${pageId}/${total}`, { headers })
+    .then((response) => {
+      advListPage = response.data;
     });
-    return advListPage
+  return advListPage;
 };
 
 export const getAdv = async (token, id) => {
-    let newAdv = null;
-    let headers = {
-        'Authorization': `Bearer ${token}`,
-    }
-    const result = await javaAcademyServiceInstance.get(
-        `/adv/${id}`,
-        { headers }
-    ).then((response) => {
-        newAdv = [response.data];
+  let newAdv = null;
+  let headers = {
+    Authorization: `Bearer ${token}`,
+  };
+  const result = await javaAcademyServiceInstance
+    .get(`/advertisement/${id}`, { headers })
+    .then((response) => {
+      newAdv = [response.data];
     });
-    return newAdv
+  return newAdv;
 };
 
-export const postAdvState = async (token, id, state) => {
-    let updateAdv = null;
-    let headers = {
-        'Authorization': `Bearer ${token}`,
-    }
-    const result = await javaAcademyServiceInstance.post(
-        `/${state}/${id}`,
-        {},
-        { headers }
-    ).then((response) => {
-        updateAdv = response.data;
-    });
-    return updateAdv
-}
-export const disableAdv = async (token, id) => {
-    let disableAdv = null;
-    let headers = {
-        'Authorization': `Bearer ${token}`,
-    }
-    const result = await javaAcademyServiceInstance.put(
-        `/advertisement/disable/${id}`,
-        {},
-        { headers }
-    ).then((response) => {
-        disableAdv = response.data;
-    });
-    return disableAdv
-}
-
-
-
 export const getPendingAdvertaisement = async (token) => {
-    let headers = {
-        'Authorization': `Bearer ${token}`,
-    }
-    const result = await javaAcademyServiceInstance.get(
-        `/pending`,
-        { headers }
-    )
-        .then((response) => {
-            return response.data
-        }).catch(
-            //Error handler
-        )
-    return result
-}
+  let headers = {
+    Authorization: `Bearer ${token}`,
+  };
+  const result = await javaAcademyServiceInstance
+    .get(`checker/pending`, { headers })
+    .then((response) => {
+      return response.data;
+    })
+    .catch
+    //Error handler
+    ();
+  return result;
+};
 
 export const getRefusedAdvertaisement = async (token) => {
-    let headers = {
-        'Authorization': `Bearer ${token}`,
-    }
-    const result = await javaAcademyServiceInstance.get(
-        `/listRefused`,
-        { headers }
-    )
-        .then((response) => {
-            return response.data
-        }).catch(
-            //Error handler
-        )
-    return result
-}
+  let headers = {
+    Authorization: `Bearer ${token}`,
+  };
+  const result = await javaAcademyServiceInstance
+    .get(`admin/listRefused`, { headers })
+    .then((response) => {
+      return response.data;
+    })
+    .catch
+    //Error handler
+    ();
+  return result;
+};
 
 export const getAdvBusinessName = async (token, businessName) => {
-    let newAdvListForBusiness = null;
-    let headers = {
-        'Authorization': `Bearer ${token}`,
-    }
-    const result = await javaAcademyServiceInstance.get(
-        `/business/advertisements/${businessName}`,
-        { headers }
-    ).then((response) => {
-        newAdvListForBusiness = response.data;
+  let newAdvListForBusiness = null;
+  let headers = {
+    Authorization: `Bearer ${token}`,
+  };
+  const result = await javaAcademyServiceInstance
+    .get(`/business/advertisements/${businessName}`, { headers })
+    .then((response) => {
+      newAdvListForBusiness = response.data;
     });
-    return newAdvListForBusiness
+  return newAdvListForBusiness;
 };
 
 export const getUserAdvertisements = async (token, userId) => {
-    let headers = {
-        'Authorization': `Bearer ${token}`,
-    }
-    const result = await javaAcademyServiceInstance.get(
-        `/user/advertisements`,
-        { headers }
-    )
-        .then((response) => {
-            return response.data
-        }).catch(
-            //Error handler
-        )
-    return result
-}
+  let headers = {
+    Authorization: `Bearer ${token}`,
+  };
+  const result = await javaAcademyServiceInstance
+    .get(`/user/myAdvertisements`, { headers })
+    .then((response) => {
+      return response.data;
+    })
+    .catch
+    //Error handler
+    ();
+  return result;
+};
+
+// POST
+export const postAdvState = async (token, id, state) => {
+  let updateAdv = null;
+  let headers = {
+    Authorization: `Bearer ${token}`,
+  };
+  const result = await javaAcademyServiceInstance
+    .post(`checker/${state}/${id}`, {}, { headers })
+    .then((response) => {
+      updateAdv = response.data;
+    });
+  return updateAdv;
+};
+
+export const searchAdvByParams = async (token, pageNum, pageSize, param) => {
+  let searchAdv = null;
+  //   let headers = {
+  //     Authorization: `Bearer ${token}`,
+  //   };
+  // { headers }
+  const result = await javaAcademyServiceInstance
+    .post(`search/${pageNum}/${pageSize}`, param)
+    .then((response) => {
+      searchAdv = response.data;
+    });
+  return searchAdv;
+};
+
+// PUT
+export const disableAdv = async (token, id) => {
+  let disableAdv = null;
+  let headers = {
+    Authorization: `Bearer ${token}`,
+  };
+  const result = await javaAcademyServiceInstance
+    .put(`/admin/disableAdvertisement/${id}`, {}, { headers })
+    .then((response) => {
+      disableAdv = response.data;
+    });
+  return disableAdv;
+};
--- src/services/frontend/advertisementApi.js
+++ src/services/frontend/advertisementApi.js
@@ -1,17 +1,13 @@
-import { javaAcademyServiceInstance as client }  from "../javaAcademyService";
+import { javaAcademyServiceInstance as client } from "../javaAcademyService";
 
 // Returns all the advs that matches the filters
-const findAds = async ( filters ) => {
-    return await client.post('/find', filters )
-}
-
+const findAds = async (filters) => {
+  return await client.post("/find", filters);
+};
 
 // Returns all the cities avaible from the database
 const getCities = async () => {
-    return await client.get('/listCities')
-}
+  return await client.get("advertisement/listCities");
+};
 
-export {
-    findAds,
-    getCities
-}
+export { findAds, getCities };
