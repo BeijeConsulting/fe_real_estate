8301e821a49fd192ffed52116962321af176f538
emidimar <dimariemilio96@gmail.com>
2022-03-02 18:50:08 +0100

    fix css notFound

--- src/common/screens/NotFound.js
+++ src/common/screens/NotFound.js
@@ -6,7 +6,7 @@ const NotFound = () => {
 	return (
 		<div className="flex flex-col justify-center items-center h-screen bg-secondary font-primary">
 			<div className="container">
-				<img src={imgNotFound} />
+				<img className="imgNotFound" src={imgNotFound} />
 				<h1 className="-mt-10 text-center text-3xl font-bold color-secondary">PAGE NOT FOUND</h1>
 			</div>
 		</div>
