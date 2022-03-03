da3a7864eb0d835f0ff55cf6e0a8c6db40da317f
Gabriela Veliu <gabriela.veliu2@gmail.com>
2022-03-03 10:59:26 +0100

    Fixed checker profile and update checker

--- src/backoffice/screens/profile/Profile.js
+++ src/backoffice/screens/profile/Profile.js
@@ -1,6 +1,6 @@
 
 //import React
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { connect } from "react-redux";
 
@@ -12,7 +12,7 @@ import "antd/dist/antd.css";
 import "./profile.css"
 
 //import Api
-import { getUserById } from "../../../services/backoffice/usersApi";
+import { getUsersProfile } from "../../../services/backoffice/usersApi";
 
 //import translations 
 import { useTranslation } from "react-i18next";
@@ -21,8 +21,7 @@ import { useTranslation } from "react-i18next";
 const Profile = (props) => {
     const { t } = useTranslation()
     let navigate = useNavigate()
-    let dataAdmin = {}
-    let data = []
+    let data = {}
 
     const [state, setState] = useState({
         data: []
@@ -32,70 +31,76 @@ const Profile = (props) => {
         navigate('/admin/profile/update-profile')
     }
 
-    const getAdminData = async () => {
-        dataAdmin = await getUserById(props.admin.id, props.admin.token)
-        data = [dataAdmin]
-        setState({data})
+    const getUserData = async () => {
+        data = await getUsersProfile(props.admin.token)
+        let dataUser = [data]
+        setState({ data: dataUser })
     }
 
     useEffect(() => {
-        getAdminData() 
-       
+        getUserData()
+
     }, [])
 
     return (
-        <div className='profile-container' >
-            <span className='img-profile'></span>
-            <div className='info-profile-box'>
-                <List
-                    className="info-list"
-                    size="small"
-                    header={<h4 className="info-profile-title">{t("BoProfile.Info.Info")}</h4>}
-                    itemLayout="vertical"
-                    dataSource={state.data}
-                    renderItem={item => (
-                        <List.Item
-                            className="info-profile-items"
-                        >
-                            <List.Item.Meta
-                                title={t("BoProfile.Info.Name")}
-                                description={item.name}
-                            />
-                            <List.Item.Meta
-                                title={t("BoProfile.Info.Surname")}
-                                description={item.surname}
-                            />
-                            <List.Item.Meta
-                                title={t("BoProfile.Info.Username")}
-                                description={item.username}
-                            />
-                        </List.Item>
-                    )}
-                />
-            </div>
+        <>
+            {
+                state.data &&
+                <div className='profile-container' >
+                    <span className='img-profile'></span>
+                    <div className='info-profile-box'>
+                        <List
+                            className="info-list"
+                            size="small"
+                            header={<h4 className="info-profile-title">{t("BoProfile.Info.Info")}</h4>}
+                            itemLayout="vertical"
+                            dataSource={state.data}
+                            renderItem={item => (
+                                <List.Item
+                                    className="info-profile-items"
+                                >
+                                    <List.Item.Meta
+                                        title={t("BoProfile.Info.Name")}
+                                        description={item.name}
+                                    />
+                                    <List.Item.Meta
+                                        title={t("BoProfile.Info.Surname")}
+                                        description={item.surname}
+                                    />
+                                    <List.Item.Meta
+                                        title={t("BoProfile.Info.Username")}
+                                        description={item.username}
+                                    />
+                                </List.Item>
+                            )}
+                        />
+                    </div>
 
-            <div className='contacts-profile-box'>
-                <List
-                    className="contacts-list"
-                    header={<h3 className='contacts-title'>{t("BoProfile.Contacts.Contacts")}</h3>}
-                    itemLayout="vertical"
-                    dataSource={state.data}
-                    renderItem={item => (
-                        <List.Item>
-                            <List.Item.Meta
-                                title={t("BoProfile.Contacts.PersonalEmail")}
-                                description={item.email}
-                            />
-                        </List.Item>
-                    )}
-                />
-            </div>
-            <Button
-                className='button-profile'
-                type="primary"
-                onClick={handleClick}
-            >{t("BoProfile.EditProfile")}</Button>
-        </div >
+                    <div className='contacts-profile-box'>
+                        <List
+                            className="contacts-list"
+                            header={<h3 className='contacts-title'>{t("BoProfile.Contacts.Contacts")}</h3>}
+                            itemLayout="vertical"
+                            dataSource={state.data}
+                            renderItem={item => (
+                                <List.Item>
+                                    <List.Item.Meta
+                                        title={t("BoProfile.Contacts.PersonalEmail")}
+                                        description={item.email}
+                                    />
+                                </List.Item>
+                            )}
+                        />
+                    </div>
+                    <Button
+                        className='button-profile'
+                        type="primary"
+                        onClick={handleClick}
+                    >{t("BoProfile.EditProfile")}</Button>
+                </div >
+            }
+        </>
+
     )
 }
 
