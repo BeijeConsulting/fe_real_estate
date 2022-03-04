196ba7093fc5d3c7b2a4f471d6e81739bee3ed2d
Gabriela Veliu <gabriela.veliu2@gmail.com>
2022-03-04 16:18:40 +0100

    Code cleaning

--- src/common/components/funcComponents/Helmet.js
+++ src/common/components/funcComponents/Helmet.js
@@ -4,7 +4,7 @@ import { Helmet as ReactHelmet } from "react-helmet";
 const Helmet = () => {
 	return (
 		<ReactHelmet>
-			<meta charset="utf-8" />
+			<meta charSet="utf-8" />
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
 			<meta
 				name="description"
